8641aeaed93019b84f68a93e576f49103c1a0365
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-08-18 16:31:53 +0600

    Frontend Template Mastering

---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,54 @@
+<footer id="footer" class="footer dark-background">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="footer-grad">
+              <h2>Important Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Resume</a></li>
+                <li><a href="">Portfolio</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="footer-grad">
+              <h2>Contact Info</h2>
+              <ul class="list-unstyled">
+                <li><a href="">info@example.com</a></li>
+                <li><a href="">contact@example.com</a></li>
+                <li><a href="">+1 5589 55488 55</a></li>
+                <li><a href="">+1 6678 254445 41</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="footer-grad">
+              <h2>Contact Info</h2>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga nesciunt odit reiciendis voluptates explicabo placeat consectetur facilis cupiditate similique repellat quisquam temporibus, iste blanditiis, iure, suscipit quis assumenda quasi nulla.</p>
+              <form class="d-flex position-relative">
+                <input
+                  class="form-control me-2"
+                  type="search"
+                  placeholder="Email..."
+                />
+                <button class="btn btn-primary position-absolute" type="submit">
+                  Send
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <div class="copyright text-center">
+          <p>
+            © <span>Copyright</span>
+            <strong class="sitename">2025</strong>
+            <span>All Rights Reserved</span>
+          </p>
+        </div>
+      </div>
+    </footer>
---
+++ resources/views/frontend/layouts/menu.blade.php
@@ -0,0 +1,40 @@
+<header id="header" class="header d-flex align-items-center fixed-top">
+      <div
+        class="container-fluid position-relative d-flex align-items-center justify-content-between"
+      >
+        <nav id="navmenu" class="navmenu">
+          <div class="profile-img">
+            <img
+              src="{{asset('frontend/assets/img/profile/profile-square-1.webp')}}"
+              alt=""
+              class="img-fluid rounded-circle"
+            />
+          </div>
+
+          <a
+            href="index.html"
+            class="logo d-flex align-items-center justify-content-center active"
+          >
+            <h1 class="sitename">Alex Smith</h1>
+          </a>
+
+          <div class="social-links text-center">
+            <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
+            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+            <a href="#" class="google-plus"><i class="bi bi-skype"></i></a>
+            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+          </div>
+
+          <ul>
+            <li><a href="#hero">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#resume">Resume</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+          <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
+      </div>
+    </header>
