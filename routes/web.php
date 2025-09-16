c0bf3f61b5a17d96a2bd727b01eebd96b1c1f1ad
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-09-16 19:58:22 +0600

    Optimize Web Route

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ use App\Http\Controllers\Admin\SubscriberController;
 use App\Http\Controllers\Admin\TagController;
 use App\Http\Controllers\Admin\TestimonialController;
 use App\Http\Controllers\Frontend\FrontendController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/dashboard', function () {
@@ -66,57 +65,81 @@ Route::middleware('auth')->prefix('admin')->as('admin.')->group(function () {
     });
 
     // Frontend Skill
-    Route::put('/skill-card-title-update/{id}', [FrontendSkillController::class, 'skillCardTitleUpdate'])->name('skill-card-title-update');
+    Route::controller(FrontendSkillController::class)->group(function () {
+        Route::put('/skill-card-title-update/{id}', 'skillCardTitleUpdate')->name('skill-card-title-update');
+    });
     Route::resource('frontend-skill', FrontendSkillController::class);
 
     // Backend Skill
-    Route::put('/backend-skill-card-title-one-update/{id}', [BackendSkillController::class, 'backendSkillCardTitleUpdate'])->name('backend-skill-card-title-one-update');
+    Route::controller(BackendSkillController::class)->group(function () {
+        Route::put('/backend-skill-card-title-one-update/{id}', 'backendSkillCardTitleUpdate')->name('backend-skill-card-title-one-update');
+    });
     Route::resource('backend-skill', BackendSkillController::class);
 
     // Design Skill
-    Route::put('/design-skill-card-title-update/{id}', [DesignSkillController::class, 'designSkillCardTitleUpdate'])->name('design-skill-card-title-update');
+    Route::controller(DesignSkillController::class)->group(function () {
+        Route::put('/design-skill-card-title-update/{id}', 'designSkillCardTitleUpdate')->name('design-skill-card-title-update');
+    });
     Route::resource('design-skill', DesignSkillController::class);
 
     // Cloud Skill
-    Route::put('/cloud-skill-card-title-update/{id}', [CloudSkillController::class, 'cloudSkillCardTitleUpdate'])->name('cloud-skill-card-title-update');
+    Route::controller(CloudSkillController::class)->group(function () {
+        Route::put('/cloud-skill-card-title-update/{id}', 'cloudSkillCardTitleUpdate')->name('cloud-skill-card-title-update');
+    });
     Route::resource('cloud-skill', CloudSkillController::class);
 
     // Certification
-    Route::put('/professional-expertise-title-update', [CertificationController::class, 'professionalExpertiseTitleUpdate'])->name('professional-expertise-title-update');
+    Route::controller(CertificationController::class)->group(function () {
+        Route::put('/professional-expertise-title-update', 'professionalExpertiseTitleUpdate')->name('professional-expertise-title-update');
+    });
     Route::resource('certification', CertificationController::class);
 
     // Professional Journey
-    Route::put('/professional-journey-title-update', [ProfessionalJourneyController::class, 'professionalJourneyTitleUpdate'])->name('professional-journey-title-update');
-    Route::put('resume/main-title/update', [ProfessionalJourneyController::class, 'resumeMainTitleUpdate'])->name('resume.main-title.update');
+    Route::controller(ProfessionalJourneyController::class)->group(function () {
+        Route::put('/professional-journey-title-update', 'professionalJourneyTitleUpdate')->name('professional-journey-title-update');
+        Route::put('resume/main-title/update', 'resumeMainTitleUpdate')->name('resume.main-title.update');
+    });
     Route::resource('professional-journey', ProfessionalJourneyController::class);
 
     // Academic Excellence
-    Route::put('/academic-excellences-title-update', [AcademicExcellenceController::class, 'academicExcellencesTitleUpdate'])->name('academic-excellences-title-update');
+    Route::controller(AcademicExcellenceController::class)->group(function () {
+        Route::put('/academic-excellences-title-update', 'academicExcellencesTitleUpdate')->name('academic-excellences-title-update');
+    });
     Route::resource('academic-excellence', AcademicExcellenceController::class);
 
     // Service
-    Route::put('services/main-title/update', [ServiceController::class, 'servicesMainTitleUpdate'])->name('services.main-title.update');
+    Route::controller(ServiceController::class)->group(function () {
+        Route::put('services/main-title/update', 'servicesMainTitleUpdate')->name('services.main-title.update');
+    });
     Route::resource('service', ServiceController::class);
 
     // Category
-    Route::put('portfolio/main-title/update', [CategoryController::class, 'portfolioMainTitleUpdate'])->name('portfolio.main-title.update');
+    Route::controller(CategoryController::class)->group(function () {
+        Route::put('portfolio/main-title/update', 'portfolioMainTitleUpdate')->name('portfolio.main-title.update');
+    });
     Route::resource('category', CategoryController::class);
 
     // Portfolio
     Route::resource('portfolio', PortfolioController::class);
 
     // Testimonial
-    Route::put('testimonial/main-title/update', [TestimonialController::class, 'testimonialMainTitleUpdate'])->name('testimonial.main-title.update');
+    Route::controller(TestimonialController::class)->group(function () {
+        Route::put('testimonial/main-title/update', 'testimonialMainTitleUpdate')->name('testimonial.main-title.update');
+    });
     Route::resource('testimonial', TestimonialController::class);
 
     // Faq
-    Route::put('faq/main-title/update', [FaqController::class, 'faqMainTitleUpdate'])->name('faq.main-title.update');
+    Route::controller(FaqController::class)->group(function () {
+        Route::put('faq/main-title/update', 'faqMainTitleUpdate')->name('faq.main-title.update');
+    });
     Route::resource('faq', FaqController::class);
 
     // Subscriber
-    Route::put('contact/main-title/update', [SubscriberController::class, 'contactMainTitleUpdate'])->name('contact.main-title.update');
-    Route::get('/subscriber-block', [SubscriberController::class, 'subscriberBlock'])->name('subscriber.block');
-    Route::post('/subscriber-sent', [SubscriberController::class, 'subscriberSent'])->name('subscriber.sent');
+    Route::controller(SubscriberController::class)->group(function () {
+        Route::put('contact/main-title/update', 'contactMainTitleUpdate')->name('contact.main-title.update');
+        Route::get('/subscriber-block', 'subscriberBlock')->name('subscriber.block');
+        Route::post('/subscriber-sent', 'subscriberSent')->name('subscriber.sent');
+    });
     Route::resource('subscriber', SubscriberController::class);
 });
 
